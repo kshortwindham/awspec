ecc21d574ddac4a01e1f40b71d3e96152e0f3949
Kevin Short <kevin.short@rackspace.com>
2018-08-01 11:30:48 -0400

    always use get_rest_apis call

--- lib/awspec/helper/finder/apigateway.rb
+++ lib/awspec/helper/finder/apigateway.rb
@@ -2,18 +2,16 @@ module Awspec::Helper
   module Finder
     module Apigateway
       def find_apigateway_by_id(id)
-        res = apigateway_client.get_rest_api({ rest_api_id: id })
-        return res if res && res.id == id
+        rest_apis = apigateway_client.get_rest_apis
+        rest_apis.items.each do |item|
+          return item if item.id == id
+        end
         nil
       end
 
       def find_apigateway_by_name(name)
-        STDOUT.puts "find_apigateway_by_name(#{name})>"
         rest_apis = apigateway_client.get_rest_apis
-        STDOUT.puts("rest_apis #{rest_apis}")
         rest_apis.items.each do |item|
-          STDOUT.puts("item #{item}")
-          STDOUT.puts("item.name #{item.name}")
           return item if item.name == name
         end
         nil
--- lib/awspec/version.rb
+++ lib/awspec/version.rb
@@ -1,3 +1,3 @@
 module Awspec
-  VERSION = '1.7.5'
+  VERSION = '1.7.6'
 end
